ba397180fea82964c172eea620d8940ca47ad82b
Lora Georgieva <loraorligeorgieva@gmail.com>
2019-09-10 21:52:04 +0300

    add solution for task3

--- src/main/java/lesson8_StringsAndMethods/Task3.java
+++ src/main/java/lesson8_StringsAndMethods/Task3.java
@@ -7,6 +7,31 @@ public class Task3 {
 
         Scanner sc = new Scanner(System.in);
 
-        // TODO
+        String firstWord = sc.nextLine();
+        String secondWord = sc.nextLine();
+
+        System.out.println(firstWord.length() == secondWord.length() ?
+                "The words have same length." : "The words have different length");
+
+        int minLength = firstWord.length();
+
+        if (firstWord.length() > secondWord.length()){
+            minLength = secondWord.length();
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < minLength; i++) {
+            char firstChar = firstWord.charAt(i);
+            char secondChar = secondWord.charAt(i);
+            if (firstChar != secondChar) {
+                if (count == 0){
+                    System.out.println("Difference by positions:");
+                }
+                count++;
+                System.out.printf("%d %c-%c%n", i + 1, firstChar, secondChar);
+            }
+        }
+
     }
 }
